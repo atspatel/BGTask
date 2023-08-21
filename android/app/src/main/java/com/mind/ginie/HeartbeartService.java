cd19c03d863a9f2d60211748e352ca775d9a0c7f
atishEM <atish@equateme.com>
2023-08-22 02:22:27 +0530

    changes for time interval

--- android/app/src/main/java/com/mind/ginie/HeartbeartService.java
+++ android/app/src/main/java/com/mind/ginie/HeartbeartService.java
@@ -28,7 +28,7 @@ public class HeartbeartService extends Service {
             Intent myIntent = new Intent(context, HeartbeatEventService.class);
             context.startService(myIntent);
             HeadlessJsTaskService.acquireWakeLockNow(context);
-            handler.postDelayed(this, 2000);
+            handler.postDelayed(this, 1*60*1000);
         }
     };
     private void createNotificationChannel() {
